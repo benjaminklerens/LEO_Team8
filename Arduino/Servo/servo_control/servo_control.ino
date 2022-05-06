8de622aadf8b516f0bfdcc419b3c1f8266f0b35b
Admir Osmanovic <adoozyman@gmail.com>
2022-05-06 13:57:12 +0200

    servo control with rfid

--- Arduino/Servo/servo_control/servo_control.ino
+++ Arduino/Servo/servo_control/servo_control.ino
@@ -7,13 +7,18 @@
 
 //libraries
 #include <ESP8266WiFiMulti.h>
+#include <SPI.h>
+#include <MFRC522.h>
 #include "Adafruit_MQTT.h"
 #include "Adafruit_MQTT_Client.h"
-#include <Servo.h>
 
 //WiFi
 #define WIFI_SSID       "LEO1_TEAM_08"
 #define WIFI_PASSWORD    "leoteam8"
+
+//RFID
+#define RST_PIN 0
+#define SS_PIN D8
 
 //mqtt
 #define MQTT_SERVER           "192.168.10.1"
@@ -26,66 +31,34 @@
 #define MQTT_TOPIC_WIFI_SNR   "/snr"
 #define MQTT_TOPIC_LED        "/led"
 
-
-//GPIO
-#define SERVO_PIN 14
-#define LED_PIN 5
-#define SOUND_SENSOR_PIN 4
-
-//timing
-#define SENSE_TIME 2000 // = Publish Time
-#define MOVE_TIME 2000
-#define SENSE_MOVE_SHIFT 1000
-#define ANGLE_STEPS 45
-
 //functions
 void debug(const char *s);
 void mqtt_connect();
 void print_wifi_status();
 void publish_data();
-void subscribe_led();
-
-
 
 //variables
 ESP8266WiFiMulti WiFiMulti;
-Servo servo;
 WiFiClient wifi_client;
+MFRC522 mfrc522(SS_PIN, RST_PIN);
+byte readCard[4];
+String MasterTag = "399DBDA3"; //UID for the blue tag
+String tagID = "";
+int rfid_status = 0;
 
 Adafruit_MQTT_Client mqtt(&wifi_client, MQTT_SERVER, MQTT_SERVERPORT, MQTT_USERNAME, MQTT_KEY);
-
-Adafruit_MQTT_Publish pos_mqtt_publish = Adafruit_MQTT_Publish(&mqtt, MQTT_USERNAME MQTT_TOPIC_SERVO_POS);
-Adafruit_MQTT_Publish sound_mqtt_publish = Adafruit_MQTT_Publish(&mqtt, MQTT_USERNAME MQTT_TOPIC_SOUND);
-Adafruit_MQTT_Publish snr_mqtt_publish = Adafruit_MQTT_Publish(&mqtt, MQTT_USERNAME MQTT_TOPIC_WIFI_SNR);
-
-Adafruit_MQTT_Subscribe status_mqtt_subscribe = Adafruit_MQTT_Subscribe(&mqtt, MQTT_USERNAME MQTT_TOPIC_STATUS);
-Adafruit_MQTT_Subscribe led_mqtt_subscribe = Adafruit_MQTT_Subscribe(&mqtt, MQTT_USERNAME MQTT_TOPIC_LED);
-
-uint32_t move_prev_time;
-uint32_t sense_prev_time;
-int16_t servo_pos = 0;
-int16_t sound = 0;
-uint8_t active_sensing = 0;
-
+Adafruit_MQTT_Publish status_mqtt_publish = Adafruit_MQTT_Publish(&mqtt, MQTT_USERNAME MQTT_TOPIC_STATUS);
 
 const uint16_t conn_tout_ms = 5000;
 
-void setup() {
-  //init time
-  move_prev_time = millis()+SENSE_MOVE_SHIFT;
-  sense_prev_time = millis();
-  //set PinMode
-  pinMode(LED_PIN,OUTPUT);
-  pinMode(SOUND_SENSOR_PIN,INPUT);
-  servo.attach(SERVO_PIN);
-  
-  //subscribe to mqtt topics
-  mqtt.subscribe(&status_mqtt_subscribe);
-  mqtt.subscribe(&led_mqtt_subscribe);
-  
+void setup() {  
   //enable serial connection and begin boot process
-  Serial.begin(115200);
-  delay(10);
+  Serial.begin(9600);
+  while (!Serial);
+  SPI.begin(); // SPI bus
+  mfrc522.PCD_Init(); // MFRC522
+  delay(100); // Optional delay. Some boards need some time after init to be ready.
+  Serial.println(F("Please scan your card/tag..."));
   debug("Boot");
   //set up wifi connection
   WiFi.persistent(false);
@@ -105,45 +78,42 @@ void setup() {
 void loop() {
   //ensure that mqtt is connected
   mqtt_connect();
-  //read topics from mqtt
-  subscribe_led();
-  
-  //reset position after sensing all positions
-  if (servo_pos > 180 - ANGLE_STEPS)
+  while (getID())
   {
-    servo_pos = 0;
-    servo.write(servo_pos);
-    move_prev_time = millis()+SENSE_MOVE_SHIFT;
-    sense_prev_time = millis();
-  }
-  
-  //check if sensing is enabled
-  if(!active_sensing)
-  {
-    move_prev_time = millis()+SENSE_MOVE_SHIFT;
-    sense_prev_time = millis();
-    
-  }else
-  {
-    //check sensing interval
-    if (millis() - move_prev_time >= SENSE_TIME)
+    if (tagID == MasterTag)
     {
-      sense_prev_time = millis();
-      sound = digitalRead(SOUND_SENSOR_PIN);
+      Serial.println(F("Access Granted!"));
+      rfid_status = 1;
       publish_data();
-    }    
-    //check movement interval
-    if (millis() - move_prev_time >= MOVE_TIME)
-    {
-      move_prev_time = millis();
-      servo_pos += ANGLE_STEPS;
-      servo.write(servo_pos);
     }
-
+    else
+    {
+      Serial.println(F("Access Denied.. SPY!"));
+      rfid_status = 0;
+      publish_data();
+    }
   }
-  
-  
+}
 
+//Read new tag if available
+boolean getID() 
+{
+  // Getting ready for Reading PICCs
+  if ( ! mfrc522.PICC_IsNewCardPresent()) { //If a new PICC placed to RFID reader continue
+  return false;
+  }
+  if ( ! mfrc522.PICC_ReadCardSerial()) { //Since a PICC placed get Serial and continue
+  return false;
+  }
+  tagID = "";
+  for ( uint8_t i = 0; i < 4; i++) { // The MIFARE PICCs that we use have 4 byte UID
+  //readCard[i] = mfrc522.uid.uidByte[i];
+  tagID.concat(String(mfrc522.uid.uidByte[i], HEX)); // Adds the 4 bytes in a single String variable
+  }
+  debug("ok were stopping it now");
+  tagID.toUpperCase();
+  mfrc522.PICC_HaltA(); // Stop reading
+  return true;
 }
 
 void mqtt_connect()
@@ -168,27 +138,17 @@ void mqtt_connect()
 
 void publish_data()
 {
-  char pos_payload[16];
-  char sound_payload[16];
-  char snr_payload[16];
-  
+  char status_payload[16];
   int snr = WiFi.RSSI();
   
-  sprintf(pos_payload, "%ld", servo_pos);
-  sprintf(sound_payload, "%ld", sound);
-  sprintf(snr_payload, "%ld", snr);
+  sprintf(status_payload, "%ld", rfid_status);
   
   Serial.print(millis());
   Serial.print(" ");
   Serial.println(" Publishing: ");
   
-  Serial.print("Position: ");
-  Serial.println(pos_payload);
-  Serial.print("Sound: ");
-  Serial.println(sound_payload);
-  Serial.print("WiFi SNR: ");
-  Serial.println(snr_payload);
-
+  Serial.print("RFID Status: ");
+  Serial.println(rfid_status);
 
   Serial.print(millis());
   Serial.print(" ");
@@ -198,84 +158,18 @@ void publish_data()
   {
     print_wifi_status();
 
-    //publish to sound topic
-    if (! sound_mqtt_publish.publish(sound_payload))
-    {
-      debug("Sound:MQTT failed");
-    }
-    else
-    {
-      debug("Sound:MQTT ok");
-    }
-
     delay(20);
-    //publish to position topic
-    if (! pos_mqtt_publish.publish(pos_payload))
+    //publish to rfid status
+    if (! status_mqtt_publish.publish(rfid_status))
     {
       debug("Position:MQTT failed");
     }
     else
     {
       debug("Position:MQTT ok");
-    }
-
-    //publish to snr topic
-    if (! snr_mqtt_publish.publish(snr_payload))
-    {
-      debug("SNR:MQTT failed");
-    }
-    else
-    {
-      debug("SNR:MQTT ok");
-    }
-    
+    }    
   }
 }
-
-void subscribe_led()
-{
-  int16_t sub_timeout = 10;
-  Adafruit_MQTT_Subscribe* sub;
-  
-  if((WiFiMulti.run(conn_tout_ms) == WL_CONNECTED))
-  {   
-    while ((sub = mqtt.readSubscription(10))) 
-    {
-      if (sub == &led_mqtt_subscribe)
-      {
-        Serial.print("Received LED Mode: ");
-        Serial.println((char*)led_mqtt_subscribe.lastread);         
-        // Switch on the LED if an 1 was received as first character
-        if ((int)(led_mqtt_subscribe.lastread[0]) == 49)
-        {
-          digitalWrite(LED_PIN, HIGH);
-          
-        }
-        if (led_mqtt_subscribe.lastread[0] == 48) 
-        {
-          digitalWrite(LED_PIN, LOW);
-        }
-      }
-
-      if (sub == &status_mqtt_subscribe)
-      {
-        Serial.print("Received Status Message: ");
-        Serial.println((char*)status_mqtt_subscribe.lastread);      
-        //Setting system active depending on received message
-        if ((int)(status_mqtt_subscribe.lastread[0]) == 49)
-        {
-          active_sensing = 1;  
-        }
-        if (status_mqtt_subscribe.lastread[0] == 48) 
-        {
-          active_sensing = 0;
-        }
-
-      }
-    }
-  }
-}
-
 
 void debug(const char *s)
 {
