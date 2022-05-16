f5146c2411cfa9345d54657b368170053fcbdeed
Erik Winkler <erik-winkler@outlook.de>
2022-05-16 16:55:40 +0200

    Adapted to sound

--- led_sound.sh
+++ led_sound.sh
@@ -1,8 +1,8 @@
 #! bin/bash
-battery=$(mosquitto_sub -d -h localhost -p 1883 -u leoteam8 -P leoteam8 -C 1 -t leoteam8/voltage | grep -v "Client" | grep -v "Subscribed")
-threshold=2500
+sound=$(mosquitto_sub -d -h localhost -p 1883 -u leoteam8 -P leoteam8 -C 1 -t leoteam8/sound | grep -v "Client" | grep -v "Subscribed")
+threshold=700
 
-if [ $battery -lt $threshold ]
+if [ $sound -lt $threshold ]
 then
 mosquitto_pub -h localhost -p 1883 -u leoteam8 -P leoteam8 -t leoteam8/led -m 0
 else
